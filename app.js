a6f47ba10955f1822aa7cbb221e6dee2e7237fbb
hammerwhammer <127695173+hammerwhammer@users.noreply.github.com>
2023-03-19 16:39:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 console.log("Hello, World!")
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form")
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
-        const username = document.querySelector("input").value
-        
-        const response = await fetch(`https://api.github.com/users/${username}`)
-        const data = await response.json()
-        console.log(data)
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault()
+      const username = document.querySelector('input').value
+  
+      const response = await fetch(`https://api.github.com/users/${username}`)
+      const data = await response.json()
+      console.log(data)
     })
-    
-})
-
+  })
