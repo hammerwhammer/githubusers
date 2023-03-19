d7fac476e2f9d9e9be76e863f8181110c4972046
hammerwhammer <127695173+hammerwhammer@users.noreply.github.com>
2023-03-19 16:36:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", (event) => {
         event.preventDefault()
         const username = document.querySelector("input").value
-        alert(username)
+        
+        const response = fetch(`https://api.github.com/users/${username}`)
+        const data = await response.json()
+        console.log(data)
     })
     
 })
