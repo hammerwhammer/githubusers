1670274385fcdbaad4850f8a105f30f9ec420ab3
hammerwhammer <127695173+hammerwhammer@users.noreply.github.com>
2023-03-19 16:30:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!")
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        alert("Pressed button!")
+    })
+    
+})
